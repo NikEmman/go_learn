6bedef1f9f5e0230fab47cbee3fdad05bedc8dfb
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2025-05-01 19:52:55 +0300

    Strings, runes, bytes

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,31 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	intArr := [...]int32{1, 2, 3}
-	fmt.Println((intArr))
-
-	var intSlice []int32 = []int32{4, 5, 6}
-
-	fmt.Printf("The length is %v with capacity %v \n", len(intSlice), cap(intSlice))
-	intSlice = append(intSlice, 7)
-	fmt.Printf("The length is %v with capacity %v \n", len(intSlice), cap(intSlice))
-	var intSlice2 []int32 = []int32{8, 9}
-	intSlice = append(intSlice, intSlice2...)
-	fmt.Printf("The length is %v with capacity %v \n", len(intSlice), cap(intSlice))
-
-	var myMap map[string]uint8 = make(map[string]uint8)
-	fmt.Println((myMap))
-	var myMap2 = map[string]uint8{"Adam": 28, "Sarah": 45}
-	fmt.Println(myMap2)
-	for name, age := range myMap2 {
-		fmt.Printf("Name: %v, Age: %v", name, age)
+	var myString = "mixedCharsλξω"
+	var indexed = myString[0]
+	fmt.Printf("%v,%T\n", indexed, indexed)
+	for i, v := range myString {
+		fmt.Println(i, v)
 	}
-	for i, v := range intArr {
-		fmt.Printf("Index:%v, Value:%v", i, v)
+
+	var strSlice = []string{"l", "o", "c", "a", "l"}
+	var catStr = ""
+	//this way we are creating a new string on each iteration, which is inefficient
+	for i := range strSlice {
+		catStr += strSlice[i]
 	}
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	fmt.Printf("%v\n", catStr)
+	// a better way to to use built-in string Builder method
+	var strBuilder strings.Builder
+	for i := range strSlice {
+		strBuilder.WriteString(strSlice[i])
 	}
+	var catStr2 = strBuilder.String()
+	fmt.Printf("%v\n", catStr2)
 }
---
+++ cmd/tutorial_1/main1.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main1() {
+	intArr := [...]int32{1, 2, 3}
+	fmt.Println((intArr))
+
+	var intSlice []int32 = []int32{4, 5, 6}
+
+	fmt.Printf("The length is %v with capacity %v \n", len(intSlice), cap(intSlice))
+	intSlice = append(intSlice, 7)
+	fmt.Printf("The length is %v with capacity %v \n", len(intSlice), cap(intSlice))
+	var intSlice2 []int32 = []int32{8, 9}
+	intSlice = append(intSlice, intSlice2...)
+	fmt.Printf("The length is %v with capacity %v \n", len(intSlice), cap(intSlice))
+
+	var myMap map[string]uint8 = make(map[string]uint8)
+	fmt.Println((myMap))
+	var myMap2 = map[string]uint8{"Adam": 28, "Sarah": 45}
+	fmt.Println(myMap2)
+	for name, age := range myMap2 {
+		fmt.Printf("Name: %v, Age: %v", name, age)
+	}
+	for i, v := range intArr {
+
+		fmt.Printf("Index:%v, Value:%v", i, v)
+	}
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
